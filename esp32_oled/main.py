b286a78e264e06ef485e2b504499cf3af3cbb2bf
Peter <xtiqin@163.com>
2022-07-14 17:39:35 +0800

    Update main.py

--- esp32_oled/main.py
+++ esp32_oled/main.py
@@ -1,34 +1,17 @@
-from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
-from DHT22 import DHT22
-import utime
-
-DHT11_PIN = 22
+import utime,machine
 
 OLED_WIDTH  = 128
-OLED_HEIGHT = 32
-
-sda=machine.Pin(4)
-scl=machine.Pin(5)
-i2c=machine.I2C(0,sda=sda, scl=scl, freq=400000)
-
-
-from ssd1306 import SSD1306_I2C
-oled = SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c)
+OLED_HEIGHT = 64
 
 
 if __name__ == "__main__":
-    dht_data = Pin(DHT11_PIN,Pin.IN,Pin.PULL_UP)
-    dht_sensor=DHT22(dht_data,dht11=True)
-    while True:
-        temperature,humidity = dht_sensor.read()
-        if temperature is None or humidity is None:
-            # skip errors
-            pass
-        else:
-            oled.fill(0)
-            oled.text(f"T: {temperature}'C",0,0)
-            oled.text(f"H: {humidity}%",0,15)
-            oled.show()
-        
-        utime.sleep(2)
+    sda=machine.Pin(4)
+    scl=machine.Pin(5)
+    i2c=machine.I2C(0,sda=sda, scl=scl, freq=10000)
+
+
+    oled = SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c)
+    oled.fill(0)
+    oled.text(f"Hello World",0,0)
+    oled.show()
