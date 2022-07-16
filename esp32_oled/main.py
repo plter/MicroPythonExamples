e2966c159cd7acd78e9b111aa9128f4764a2fb61
Peter <xtiqin@163.com>
2022-07-17 07:58:08 +0800

    update

--- esp32_oled/main.py
+++ esp32_oled/main.py
@@ -1,17 +1,15 @@
 from ssd1306 import SSD1306_I2C
-import utime,machine
+import utime, machine
 
-OLED_WIDTH  = 128
+OLED_WIDTH = 128
 OLED_HEIGHT = 64
 
-
 if __name__ == "__main__":
-    sda=machine.Pin(4)
-    scl=machine.Pin(5)
-    i2c=machine.I2C(0,sda=sda, scl=scl, freq=10000)
-
+    sda = machine.Pin(4)
+    scl = machine.Pin(5)
+    i2c = machine.I2C(0, sda=sda, scl=scl, freq=10000)
 
     oled = SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c)
     oled.fill(0)
-    oled.text(f"Hello World",0,0)
+    oled.text(f"Hello World", 0, 0)
     oled.show()
