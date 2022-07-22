b638da181dd6e22afb668e83db5d46b438b47a6c
Peter <xtiqin@163.com>
2022-07-22 19:11:14 +0800

    Update main.py

--- esp32c3_blink/main.py
+++ esp32c3_blink/main.py
@@ -1,4 +1,16 @@
 from machine import Pin
+import uasyncio
 
-p = Pin(4, Pin.OUT)
-p.on()
+
+async def main():
+    p = Pin(12, Pin.OUT)
+    while True:
+        p.on()
+        await uasyncio.sleep(1)
+        p.off()
+        await uasyncio.sleep(1)
+    pass
+
+
+if __name__ == '__main__':
+    uasyncio.run(main())
